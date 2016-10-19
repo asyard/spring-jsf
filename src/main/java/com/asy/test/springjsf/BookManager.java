2bc990aa75f58f56a2297e62507d9df597d4a20c
asyard <asyard@gmail.com>
2016-10-19 11:12:55 +0300

    stuff about books

---
+++ src/main/java/com/asy/test/springjsf/BookManager.java
@@ -0,0 +1,65 @@
+package com.asy.test.springjsf;
+
+import com.asy.test.springjsf.model.Book;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by asy
+ */
+
+@ManagedBean(name = "bookManager")
+@RequestScoped
+public class BookManager {
+
+    private String name;
+    private List<Book> books;
+    private List<Book> filteredBooks;
+    private Book selectedBook;
+
+    @PostConstruct
+    public void init() {
+        books = new ArrayList<Book>();
+        books.add(new Book("A", "b", 1441));
+        books.add(new Book("A2", "b2", 1442));
+        books.add(new Book("A3", "b3", 1443));
+        books.add(new Book("A4", "b4", 1444));
+        books.add(new Book("A5", "b5", 1445));
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public List<Book> getFilteredBooks() {
+        return filteredBooks;
+    }
+
+    public String doSave() {
+        System.out.println("------------ " + name + " ----------------------");
+        return "menu";
+    }
+
+    public Book getSelectedBook() {
+        return selectedBook;
+    }
+
+    public void setSelectedBook(Book selectedBook) {
+        this.selectedBook = selectedBook;
+    }
+
+
+}
